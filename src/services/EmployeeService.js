e07d5db582208c191162869f6d7b988842e7f062
osmanforhad <developer.osmanforhad@gmail.com>
2022-05-25 10:58:20 +0600

    connect react js front end with update employee rest api

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -2,18 +2,22 @@ import axios from "axios";
 
 const API_URL = "http://localhost:8080/api/v1/";
 
-class EmployeeService{
+class EmployeeService {
 
-    getEmployees(){
+    getEmployees() {
         return axios.get(API_URL + "employees");
     }
 
-    addEmployee(employee){
+    addEmployee(employee) {
         return axios.post(API_URL + "create_employee", employee);
     }
 
-    getEmployeeById(employeeId){
-        return axios.get(API_URL + "employee" + "/" + employeeId);
+    getEmployeeById(employeeId) {
+        return axios.get(API_URL + "employee/" + employeeId);
+    }
+
+    updateEmployee(employee, employeeId) {
+        return axios.put(API_URL + "employee/" + employeeId, employee);
     }
 
 }
